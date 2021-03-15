d4e9663b851896e8025ae98b5a52027ddf77ea5d
Leonard Grosoli <hileo77@gmail.com>
2021-03-15 14:09:55 +0100

    streamLined Adding apps.

--- mod.ts
+++ mod.ts
@@ -2,6 +2,26 @@ import { action, error } from "./log.ts";
 import { getAppsInfo, getDisplayInfo, moveWindow, moveWindowDisplay, sleep } from "./utils.ts";
 
 
+//           name    desktop display
+const apps: [string, number, number][] = [
+  ['spotify.Spotify', 2, 1],
+  ['discord.discord', 2, 0],
+  ['github desktop.GitHub Desktop', 1, 1]
+]
+
+let searchForApps = true
+while (searchForApps) {
+  const appsInfo = await getAppsInfo()
+  let foundApp = true
+  for (let i = 0; i < apps.length; i++) {
+    const app = appsInfo.find((item) => item.name === apps[i][0]);
+    foundApp = foundApp && !!app
+  }
+  searchForApps = !foundApp
+  await sleep(500)
+}
+action('Found Apps')
+
 const displayInfo = await getDisplayInfo()
 const appsInfo = await getAppsInfo()
 
@@ -9,19 +29,21 @@ async function findAndMove(name: string, desktop: number, displayIndex: number) 
   const app = appsInfo.find((val) => val.name === name)
   if (app) {
     await moveWindow(app.id, desktop)
-    action('Moved', name, 'to', '' + desktop)
+    action('Moved', name, 'to', 'desktop: ' + desktop)
 
     const display = displayInfo.getDisplay(displayIndex)
     if (display) {
       await moveWindowDisplay(app.id, display.position)
-      action('Set Display of', name, 'to', '' + display.name)
+      action('Moved', name, 'to', 'display: ' + display.name)
     }
     return
   }
   error('Could not move', name, 'to', '' + desktop)
 }
 
-await sleep(1000)
+for (let i = 0; i < apps.length; i++) {
+  const app = apps[i];
+  await findAndMove(...app)
+}
 
-await findAndMove('spotify.Spotify', 2, 1)
-await findAndMove('github desktop.GitHub Desktop', 1, 1)
+action('Started Apps')
